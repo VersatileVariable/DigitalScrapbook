29946d35d32238556effe869708d1d3c97dced54
Anna Galeano <wanderingonwater@gmail.com>
2025-05-21 18:51:24 +0200

    Initial Commit

---
+++ Digital Scrapbook/Digital_ScrapbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Digital_ScrapbookApp.swift
+//  Digital Scrapbook
+//
+//  Created by Anna Galeano on 5/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Digital_ScrapbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Digital ScrapbookTests/Digital_ScrapbookTests.swift
@@ -0,0 +1,16 @@
+//
+//  Digital_ScrapbookTests.swift
+//  Digital ScrapbookTests
+//
+//  Created by Anna Galeano on 5/21/25.
+//
+
+import Testing
+
+struct Digital_ScrapbookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
